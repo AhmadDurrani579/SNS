f9504690e7e010293fb3c7b6e6f57f153a985838
Ahmad <ahmaddurranitrg@gmail.com>
2019-04-23 23:23:44 +0400

    Initial Commit

---
+++ SNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by Ahmed Durrani on 23/04/2019.
+//  Copyright © 2019 CyberHost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
